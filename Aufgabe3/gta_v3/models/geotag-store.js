9c9a5b5675ca8117664cc38a92442883412f0ae1
Moritz Gartner <gartnermoritz@yahoo.de>
2022-05-22 16:56:28 +0200

    Add backend functionality to manage GeoTags

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -7,26 +7,53 @@
 
 /**
  * A class for in-memory-storage of geotags
- * 
+ *
  * Use an array to store a multiset of geotags.
  * - The array must not be accessible from outside the store.
- * 
+ *
  * Provide a method 'addGeoTag' to add a geotag to the store.
- * 
+ *
  * Provide a method 'removeGeoTag' to delete geo-tags from the store by name.
- * 
+ *
  * Provide a method 'getNearbyGeoTags' that returns all geotags in the proximity of a location.
  * - The location is given as a parameter.
  * - The proximity is computed by means of a radius around the location.
- * 
+ *
  * Provide a method 'searchNearbyGeoTags' that returns all geotags in the proximity of a location that match a keyword.
  * - The proximity constrained is the same as for 'getNearbyGeoTags'.
- * - Keyword matching should include partial matches from name or hashtag fields. 
+ * - Keyword matching should include partial matches from name or hashtag fields.
  */
-class InMemoryGeoTagStore{
+class InMemoryGeoTagStore {
+  _geoTags = [];
 
-    // TODO: ... your code here ...
+  constructor() {
+    GeoTagExamples.tagList.forEach((geoTag) => {
+      this._geoTags.push(new GeoTag(...geoTag));
+    });
+  }
 
+  addGeoTag(geoTag) {
+    this._geoTags.push(geoTag);
+  }
+
+  removeGeoTag(name) {
+    this._geoTags = this._geoTags.filter((geoTag) => geoTag.getName() !== name);
+  }
+
+  getNearbyGeoTags(longitude, latitude, radius) {
+    return this._geoTags.filter((geoTag) => {
+      const distance = geoTag.getDistanceTo(longitude, latitude);
+      return distance <= radius;
+    });
+  }
+
+  searchNearbyGeoTags(longitude, latitude, radius, keyword) {
+    return this.getNearbyGeoTags(longitude, latitude, radius).filter(
+      (geoTag) =>
+        geoTag.getName().includes(keyword) ||
+        geoTag.getHashtag().includes(keyword)
+    );
+  }
 }
 
-module.exports = InMemoryGeoTagStore
+module.exports = InMemoryGeoTagStore;
--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -5,14 +5,47 @@
  * Complete all TODOs in the code documentation.
  */
 
-/** * 
+/** *
  * A class representing geotags.
  * GeoTag objects should contain at least all fields of the tagging form.
  */
 class GeoTag {
+  _name = "";
+  _latitude = 0.0;
+  _longitude = 0.0;
+  _hashtag = "";
 
-    // TODO: ... your code here ...
-    
+  constructor(name, latitude, longitude, hashtag) {
+    this._name = name;
+    this._latitude = latitude;
+    this._longitude = longitude;
+    this._hashtag = hashtag;
+  }
+
+  getLatitude() {
+    return this._latitude;
+  }
+
+  getLongitude() {
+    return this._longitude;
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  getHashtag() {
+    return this._hashtag;
+  }
+
+  getDistanceTo(longitude, latitude) {
+    const x1 = this._longitude;
+    const y1 = this._latitude;
+    const x2 = longitude;
+    const y2 = latitude;
+    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    return distance;
+  }
 }
 
 module.exports = GeoTag;
